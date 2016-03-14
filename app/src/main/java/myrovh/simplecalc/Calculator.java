447a22673001ce9fe861043ef65154a92c35dad7
myrovh <myrovh@yandex.com>
2016-03-14 21:50:40 +1100

    calculator class fully implemented

--- app/src/main/java/myrovh/simplecalc/Calculator.java
+++ app/src/main/java/myrovh/simplecalc/Calculator.java
@@ -43,12 +43,41 @@ public class Calculator {
             case '*': currentOperation = Operation.MULTIPLY;
                 break;
             default: currentOperation = Operation.NONE;
-
         }
     }
     // Default Getters and Setters END
 
+    public float calculate() {
+        if(currentOperation == Operation.SUM) {
+            return sumNumbers();
+        }
+        else if(currentOperation == Operation.SUBTRACT) {
+            return subtractNumbers();
+        }
+        else if(currentOperation == Operation.DIVIDE) {
+            return divideNumbers();
+        }
+        else if(currentOperation == Operation.MULTIPLY) {
+            return multiplyNumbers();
+        }
+        else {
+            return 0;
+        }
+    }
+
     public float sumNumbers() {
         return numberOne + numberTwo;
+    }
+
+    public float subtractNumbers() {
+        return  numberOne - numberTwo;
+    }
+
+    public float divideNumbers() {
+        return numberOne / numberTwo;
+    }
+
+    public  float multiplyNumbers() {
+        return numberOne * numberTwo;
     }
 }
